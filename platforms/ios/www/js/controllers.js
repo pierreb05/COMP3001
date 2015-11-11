1b0acea3c2d4abfa66ea0eddc5a483461f81ab2f
pierreb05 <pierrebarreau05@gmail.com>
2015-11-11 19:04:02 +0000

    Changed menu icons to custom icons

---
+++ platforms/ios/www/js/controllers.js
@@ -0,0 +1,29 @@
+angular.module('app.controllers', [])
+
+.controller('homeCtrl', function($scope) {
+
+})
+
+.controller('pollutionMapCtrl', function($scope) {
+ $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+})
+
+.controller('settingsCtrl', function($scope) {
+
+})
+
+.controller('directionCtrl', function($scope) {
+ $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+})
+
+.controller('newWorkoutCtrl', function($scope) {
+
+})
+
+.controller('customWorkoutCtrl', function($scope) {
+
+})
+
+.controller('workoutDetailCtrl', function($scope) {
+$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+})
---
+++ platforms/ios/www/js/routes.js
@@ -0,0 +1,85 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+    
+      
+        
+    .state('home', {
+      url: '/home',
+      templateUrl: 'templates/home.html',
+      controller: 'homeCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('pollutionMap', {
+      url: '/pollutionmap',
+      templateUrl: 'templates/pollutionMap.html',
+      controller: 'pollutionMapCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('settings', {
+      url: '/settings',
+      templateUrl: 'templates/settings.html',
+      controller: 'settingsCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('direction', {
+      url: '/direction',
+      templateUrl: 'templates/direction.html',
+      controller: 'directionCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('newWorkout', {
+      url: '/newworkout',
+      templateUrl: 'templates/newWorkout.html',
+      controller: 'newWorkoutCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('customWorkout', {
+      url: '/customworkout',
+      templateUrl: 'templates/customWorkout.html',
+      controller: 'customWorkoutCtrl'
+    })
+        
+      
+    
+      
+        
+    .state('workoutDetail', {
+      url: '/detail',
+      templateUrl: 'templates/workoutDetail.html',
+      controller: 'workoutDetailCtrl'
+    })
+        
+      
+    ;
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/home');
+
+});

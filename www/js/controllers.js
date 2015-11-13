3c19d7d3c1a46296f91980497362968bddbd6b9f
jack chan <jackchan715@hotmail.com>
2015-11-13 15:32:18 +0000

    add android build and improved UI for android

---
+++ www/js/controllers.js
@@ -0,0 +1,37 @@
+angular.module('app.controllers', [])
+
+.controller('homeCtrl', function($scope) {
+
+})
+
+.controller('pollutionMapCtrl', function($scope) {
+ $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+})
+
+.controller('settingsCtrl', function($scope) {
+
+})
+
+.controller('directionCtrl', function($scope) {
+ $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+})
+
+.controller('newWorkoutCtrl', function($scope) {
+
+})
+
+.controller('customWorkoutCtrl', function($scope) {
+
+})
+
+.controller('recapCtrl', function($scope) {
+
+})
+
+.controller('runCtrl', function($scope) {
+ $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+})
+
+.controller('workoutDetailCtrl', function($scope) {
+$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+})

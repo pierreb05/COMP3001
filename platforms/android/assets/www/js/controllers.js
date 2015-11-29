9e1349151ab7e6b2706f715018a3c6e9562748e5
jack chan <jackchan715@hotmail.com>
2015-11-29 19:40:39 +0000

    directions working, 10 routes shown when user inputs 2 locations

--- platforms/android/assets/www/js/controllers.js
+++ platforms/android/assets/www/js/controllers.js
@@ -5,11 +5,19 @@ angular.module('app.controllers', [])
 })
 
 .controller('pollutionMapCtrl', function($scope) {
- $scope.map = { center: { latitude: 51.508742, longitude: -0.120850 }, zoom: 8 };
+ /*$scope.map = { center: { latitude: 51.508742, longitude: -0.120850 }, zoom: 8 };
  $scope.updateMap = function (lat, lng) {
- 		//var lat = pos.geometry.location.lat();
- 		//var lng = pos.geometry.location.lng();
-		$scope.map = { center: { latitude: lat, longitude: lng }, zoom: 8 };
+		$scope.map = { center: { latitude: lat, longitude: lng }, zoom: 15 };
+ 	}*/
+
+ 	 $scope.$on('mapInitialized', function(event, map) {
+      map.setCenter(new google.maps.LatLng(51.5085300, -0.1257400));
+      //$scope.map.setZoom(12);
+    });
+
+ 	 $scope.updateMap = function (lat, lng) {
+		$scope.map.setCenter(new google.maps.LatLng(lat, lng));
+		$scope.map.setZoom(15);
  	}
 })
 
@@ -18,7 +26,57 @@ angular.module('app.controllers', [])
 })
 
 .controller('directionCtrl', function($scope) {
- $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+ $scope.$on('mapInitialized', function(event, map) {
+      //map.setCenter(new google.maps.LatLng(51.5085300, -0.1257400));
+      //$scope.map.setZoom(12);
+    });
+  
+
+ 	$scope.routes= [];
+
+
+  $scope.drawRoutes = function (lat1, lng1, lat2, lng2) {
+
+  	var jsonUrl = "http://luftwagen.herokuapp.com/api/directions_A_to_B?Alat="+lat1+"&Along="+lng1+"&Blat="+lat2+"&Blong="+lng2+"&format=json";
+
+  	/*var req = new XMLHttpRequest();
+  	req.open("GET", jsonUrl, false);
+  	req.send(null);
+  	var jsonText;
+  	if(req.status==200)
+  	{
+  		jsonText = req.responseText;
+  		dump(jsonText);
+  	}*/
+
+  	var jsonText = file_get_contents(jsonUrl);
+  	var obj = JSON.parse(jsonText);
+  	console.log(obj[0].coords);
+
+	$scope.routes= [
+	 	{
+	 		path: obj[0].coords
+	 	}, {
+	 		path: obj[1].coords
+	 	}, {
+	 		path: obj[2].coords
+	 	}, {
+	 		path: obj[3].coords
+	 	}, {
+	 		path: obj[4].coords
+	 	}, {
+	 		path: obj[5].coords
+	 	}, {
+	 		path: obj[6].coords
+	 	}, {
+	 		path: obj[7].coords
+	 	}, {
+	 		path: obj[8].coords
+	 	}, {
+	 		path: obj[9].coords
+	 	}
+	 ];
+ 	}
 })
 
 .controller('newWorkoutCtrl', function($scope) {
@@ -34,12 +92,12 @@ angular.module('app.controllers', [])
 })
 
 .controller('runCtrl', function($scope) {
- $scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+ $scope.map = { center: { latitude: 51.5085300, longitude: -0.1257400 }, zoom: 8 };
 })
 
 .controller('workoutDetailCtrl', function($scope) {
 
-	$scope.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+	$scope.map = { center: { latitude: 51.5085300, longitude: -0.1257400 }, zoom: 8 };
 	
 })
 
